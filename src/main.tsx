ed6d281181261ef99cae5ccf6602e26a292e5206
DARRASSI YOUNES <darrassi-you@upf.ac.ma>
2025-03-24 15:06:49 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './styles/themes.css' // Add this line before index.css
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
