05f3e8a4178383b50b3091aaee3f1d6e21a08db9
DARRASSI YOUNES <darrassi-you@upf.ac.ma>
2025-09-24 22:37:40 +0200

    Fix transform origin for top-right corner

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -21,7 +21,7 @@ const Canvas: React.FC<CanvasProps> = ({ className }) => {
       // Top-Right Corner
       position: 'top-0 left-0',
       rotation: 'rotate-90',
-            origin: 'transform-origin-left-top',
+            origin: 'transform-origin-buttom-left',
      
     },
     {
