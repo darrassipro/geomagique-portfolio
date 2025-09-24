92e74abb38568b248ee087ce774203836977bcbf
DARRASSI YOUNES <darrassi-you@upf.ac.ma>
2025-09-24 22:33:00 +0200

    Update Canvas.tsx

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -21,7 +21,7 @@ const Canvas: React.FC<CanvasProps> = ({ className }) => {
       // Top-Right Corner
       position: 'top-0 left-0',
       rotation: 'rotate-90',
-            origin: 'transform-origin-bottom-right',
+            origin: 'transform-origin-top-left',
      
     },
     {
