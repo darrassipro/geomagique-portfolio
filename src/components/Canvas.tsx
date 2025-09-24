6cd50148dc2a987c9c85441dcd659e9de676e976
DARRASSI YOUNES <darrassi-you@upf.ac.ma>
2025-09-24 22:52:45 +0200

    Fix origin property for top-left corner in Canvas
    Corrected the origin property for the top-left corner.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -11,7 +11,7 @@ const Canvas: React.FC<CanvasProps> = ({ className }) => {
 
   // Define the corners with precise transform classes for positioning and rotation
  const corners = [ { position: 'bottom-0 left-0', rotation: '-rotate-180', origin: 'transform-origin-top-left', },
-                  { position: 'top-0 left-0', rotation: 'rotate-0', origin: 'transform-origin-buttom-right', },
+                  { position: 'top-0 left-0', rotation: 'rotate-0', origin: 'transform-origin-buttom-left', },
                   { position: 'top-0 right-0', rotation: 'rotate-0', origin: 'transform-origin-top-left', },
                   { position: 'bottom-0 right-0', rotation: 'rotate-90', origin: 'transform-origin-top-right', }, 
                  
